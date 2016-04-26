20f2ed6410885db984ec2c36d50ee872bc00cc9d
sezalcruz <cesarcruz91@gmail.com>
2016-04-25 20:43:59 -0400

    Initial commit

---
+++ routes/home.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/home', function(req, res, next) {
+  console.log('getting called');
+  res.render('index');
+});
+
+module.exports = router;
