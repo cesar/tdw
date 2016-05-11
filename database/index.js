f7c906002b5e0917bd68d0170ca87b096cc3d79d
sezalcruz <cesarcruz91@gmail.com>
2016-05-11 19:07:59 -0400

    end

--- database/index.js
+++ database/index.js
@@ -4,9 +4,7 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 
 //Connect to the database
-mongoose.connect(process.env.DATABASE_URL,  {
-  replSet : {rs_name : 'rs0', readPreference : 'secundary'}
-});
+mongoose.connect(process.env.DATABASE_URL_DEV);
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
